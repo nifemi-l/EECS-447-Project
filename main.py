85651a9b740da8a12c99141b458796f997033cf9
Holden Vail <112910214+HoldenV@users.noreply.github.com>
2025-04-01 20:14:35 -0500

    initial connection code

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+Authors: Holden Vail, Michael Stang, Logan Smith, Blake Carlson, Nifemi Lawal
+Class: EECS 447
+Assignment: Database Design Project Part 5: Physical Database Design
+"""
+
+import psycopg2
+
+# Database connection parameters
+DB_HOST = "libdb-25co-postgres.cajikaswgj3d.us-east-1.rds.amazonaws.com"
+DB_NAME = ""
+DB_USER = "LibDB_25Co"
+DB_PASSWORD = "null"        # Replace this with the master password
+
+def connect_to_db():
+    """Establish a connection to the PostgreSQL database."""
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+        print("DB connection successful")
+        return conn
+    except Exception as e:
+        print(f"DB connection failure: {e}")
+        return None
+
+
+def main():
+    """CLI for interacting with DB"""
+    pass
+
+main()
