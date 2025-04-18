f3a74ae3124b5dacf65d4752b8d6db084099ae84
Michael S <mstang822@gmail.com>
2025-04-17 22:29:05 -0500

    added CLI base

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ Class: EECS 447
 Assignment: Database Design Project Part 5: Physical Database Design
 """
 
+import getpass
 import psycopg2
 
 # Database connection parameters
@@ -11,6 +12,8 @@ DB_HOST = "libdb-25co-postgres.cajikaswgj3d.us-east-1.rds.amazonaws.com"
 DB_NAME = "postgres"
 DB_USER = "LibDB_25Co"
 DB_PASSWORD = "null"        # Replace this with the master password
+
+LIBRARY_PASSWORD = "password"
 
 def connect_to_db():
     """Establish a connection to the PostgreSQL DB"""
@@ -47,15 +50,56 @@ def ensure_configured(db_connection):
     except Exception as e:
         print(f"exception encountered: {e}")
 
+def verify():
+    print("The admin password is required for that action.")
+    password_input = getpass.getpass()
+    return (password_input == LIBRARY_PASSWORD) # Beautiful security :)
+
+def execute(called_function, params, active_user, admin):
+    if admin:
+        if verify():
+            return called_function(params, active_user)
+        else:
+            print("Invalid Admin Password. Please try again!")
+    else:
+        return called_function(params, active_user)
+
+def example_command(params, active_user):
+    my_parameter_1 = int(params[0])
+    my_parameter_2 = int(params[1])
+    print(my_parameter_1 + my_parameter_2)
 
 def main():
     """CLI for interacting with DB"""
     connection  = connect_to_db()
     ensure_configured(connection)
 
-    while True:
-        input_string = input("command: ")
-        for word in input_string.split(' '):
-            print(word)
+    quit = False
+    active_user = ""
+    while not quit:
+        active_user = input("Please input your Library ID: ")
+        if not active_user.isnumeric():
+            active_user = ""
+            print("Please input a valid Library ID\n")
+
+        while active_user != "":
+            input_string = input(">> ")
+            command = input_string.split(' ')
+
+            # Insert quote connection
+
+            match command[0].lower():
+                case "quit":
+                    if verify():
+                        quit = True # end program running
+                        break
+                case "logout":
+                    active_user = ""
+                case "example":
+                    execute(called_function=example_command, params=command[1:], active_user=active_user, admin=False)
+                case "admin_example":
+                    execute(called_function=example_command, params=command[1:], active_user=active_user, admin=True)
+                case _:
+                    print("Wow")
 
 main()
